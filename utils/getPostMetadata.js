67d562b5168db4a5fc8eb00abb4b311031904afa
Juuso Rutanen <juuso.rutanen@gmail.com>
2024-03-22 17:09:19 +0200

    add function to read md files

---
+++ utils/getPostMetadata.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+
+export default function getPostMetadata(basePath) {
+    const folder = basePath + '/'
+    const files = fs.readdirSync(folder)
+    const markdownPosts = files.filter(file => file.endsWith('.md'))
+
+    // getting the file data
+    const posts = markdownPosts.map((filename) => {
+        const fileContents = fs.readFileSync(`${basePath}/${filename}`, 'utf8')
+        const matterResult = matter(fileContents)
+        return {
+            title: matterResult.data.title,
+            description: matterResult.data.description,
+            date: matterResult.data.date,
+            slug: filename.replace('.md', '')
+        }
+    })
+    return posts
+}
