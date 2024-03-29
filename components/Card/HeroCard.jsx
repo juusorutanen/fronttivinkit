7f6c33049f3aeae3237b4a6c8085ca00060c9856
Juuso Rutanen <juuso.rutanen@gmail.com>
2024-03-29 12:40:32 +0200

    modify cards to use tailwind + change language to Finnish

--- components/Card/HeroCard.jsx
+++ components/Card/HeroCard.jsx
@@ -3,21 +3,19 @@ import Image from "next/image";
 
 export default function HeroCard({post}) {
     return (
-        <div className="hero-container">
-            <section className="hero-content">
-                <h1>Discover hand-picked bite-sized tips for your development needs.</h1>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type book.
+        <div className="flex flex-row gap-4 mt-20 mb-32">
+            <section className="w-1/3 bg-light-gray flex flex-col justify-center items-center p-4 gap-5">
+                <h3 className="text-h4 text-display text-center">Löydä hyödyllisiä vinkkejä ohjelmointiin ja ohjelmointialalle.</h3>
+                <p className="ext-text mx-auto text-base text-center xl:text-h6 2xl:text-h5 pt-5 max-w-[50ch]">
+                    Yksinkertaisia ja helposti sulateltavia vinkkejä alasta kiinnostuneille. Vinkit keskittyvät pääosin frontti- ja webkehitykseen.
                 </p>
-                <span>
-                    <Link href={`/tip/${post.slug}`}>More recent tips</Link>
+                <span className="text-bg rounded-lg font-semibold bg-gradient-to-br from-gradient to-primary group sm:px-5 sm:py-3 px-3 py-2 flex items-center gap-x-1 flex-shrink-0 transition-all duration-200 text-base xl:text-h6 2xl:text-h5">
+                    <Link href={`/tip/${post.slug}`}>Viimeisimmät vinkit</Link>
                 </span>
             </section>
-            <article className="hero-featured-post">
+            <article className="w-2/3 relative">
                 <div className="hero-img-container">
-                    <div className="hero-text-overlay">
+                    <div className="hero-text-overlay text-center text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                         <h2>{post.title}</h2>
                         <p className="card-desc">{post.description}</p>
                     </div>
@@ -33,3 +31,4 @@ export default function HeroCard({post}) {
         </div>
     );
 }
+
--- components/Card/PostCard.js
+++ components/Card/PostCard.js
@@ -2,22 +2,32 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function PostCard({post}) {
-
     const formattedDate = new Date(post.date).toLocaleDateString();
+    
     return (
-        <Link className="unstyled" href={`/tip/${post.slug}`}>
-            <div className="card">
-                <div className="card-img-holder">
-                    <Image width={200} height={100} src={post.image} alt={post.title}/>
+        <article>
+        <Link href={`/tip/${post.slug}`}>
+                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-dark-charcoal text-white">
+                    <div className="relative">
+                        <Image
+                            className="w-full"
+                            width={200}
+                            height={100}
+                            src={post.image}
+                            alt={post.title}
+                        />
+                    </div>
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">{post.title}</div>
+                        <p className="text-gray-300 text-base mb-2">{post.description}</p>
+                        <p className="text-gray-500 text-sm">{formattedDate}</p>
+                    </div>
+                    <div className="px-6 py-4 flex justify-between items-center">
+                        <span className="font-bold text-blue-500 hover:text-blue-700">Read more</span>
+                        <div className="px-3 py-1 bg-blue-200 text-blue-800 rounded-full text-sm">{post.category}</div>
+                    </div>
                 </div>
-                <h3>{post.title}</h3>
-                <span className="card-date">{formattedDate}</span>
-                <p className="card-desc">{post.description}</p>
-                <div className="card-bottom-container">
-                    <span>Read more</span>
-                    <div className="card-tag">{post.category}</div>
-                </div>
-            </div>
         </Link>
-    )
+        </article>
+    );
 }
