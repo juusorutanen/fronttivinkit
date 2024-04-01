e0e107a3dac4666827d3d638cc29b6fc689a4821
Juuso Rutanen <juuso.rutanen@gmail.com>
2024-04-01 12:43:20 +0300

    feat: add category filtering

--- app/page.js
+++ app/page.js
@@ -1,19 +1,29 @@
 import getPostMetadata from "@/utils/getPostMetadata";
 import PostCard from "@/components/Card/PostCard";
 import HeroCard from "@/components/Card/HeroCard";
+import CategoryButtons from "@/components/Categories/Categories";
 
-export default function Home() {
-  const postMetadata = getPostMetadata("tips");
+export default function Home({ searchParams }) {
+  const { category } = searchParams;
 
-  const latestPost = postMetadata[0];
+  // Fetch all posts
+  const allPosts = getPostMetadata("tips");
+
+  // Filter posts based on the selected category
+  const filteredPosts = category
+    ? allPosts.filter((post) => post.category === category)
+    : allPosts;
+
+  const latestPost = allPosts[0];
 
   return (
     <main>
       <HeroCard post={latestPost} />
+      <CategoryButtons posts={allPosts} />
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {postMetadata.map((post, postIndex) => {
-          return <PostCard key={postIndex} post={post} />;
-        })}
+        {filteredPosts.map((post, postIndex) => (
+          <PostCard key={postIndex} post={post} />
+        ))}
       </div>
     </main>
   );
---
+++ components/Categories/Categories.jsx
@@ -0,0 +1,71 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { motion } from "framer-motion";
+
+export default function CategoryButtons({ posts }) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  // Extract category from the URL query
+  const activeCategory = searchParams.get("category") || "";
+
+  const categoryCount = {};
+  if (posts) {
+    posts.forEach((post) => {
+      const categoryItem = post.category;
+      categoryCount[categoryItem] = (categoryCount[categoryItem] || 0) + 1;
+    });
+  }
+
+  return (
+    // make categories to always stay at the top
+    <div className="mb-8 sticky top-6 z-50 justify-center gap-x-2 flex-wrap gap-y-2 hidden sm:flex">
+      <button
+        aria-label="All categories"
+        onClick={() => {
+          router.push("/", { scroll: false });
+        }}
+        className={`py-2 px-4 flex gap-x-1 font-medium items-center border border-dim-gray  rounded-xl hover:border-text transition-all text-sm xl:text-h6 ${
+          activeCategory === "" ? "bg-accent text-bg" : "bg-bg text-accent"
+        }`}
+      >
+        All
+      </button>
+      <div className="bg-dark-gray p-1 flex gap-x-0 bg-dark-charcoal rounded-xl border border-outline border-opacity-15">
+        {Object.entries(categoryCount).map(([category, count]) => {
+          return (
+            <button
+              key={category}
+              onClick={(e) => {
+                e.preventDefault();
+                router.push(`/?category=${category}`, { scroll: false });
+              }}
+              className={`py-1 px-7 flex gap-x-1 font-medium transition-all relative ${
+                activeCategory === category ? "" : "   hover:text-gray"
+              }`}
+            >
+                {/* use motion div to create a sliding effect */}
+              {activeCategory === category && (
+                <motion.div
+                  transition={{
+                    duration: 0.6,
+                    ease: [0.16, 1, 0.3, 1],
+                  }}
+                  layoutId="active"
+                  className=" absolute inset-0 bg-accent rounded-xl"
+                />
+              )}
+              <span className="text-sm xl:text-h6 relative mix-blend-exclusion  z-10">
+                {category}
+              </span>
+              {/* Display number of posts on top corner of each category */}
+              <span className="flex relative justify-center mix-blend-exclusion z-10 items-center text-light-gray text-xxs xl:text-xs 2xl:text-sm leading-none">
+                {count}
+              </span>
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
