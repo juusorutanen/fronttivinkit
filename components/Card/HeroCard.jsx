e7bb8813c657366d9b942f29b6d514a5b4c630d9
Juuso Rutanen <juuso.rutanen@gmail.com>
2024-04-26 16:20:46 +0300

    clean up

--- components/Card/HeroCard.jsx
+++ components/Card/HeroCard.jsx
@@ -1,21 +1,15 @@
-import Link from "next/link";
 import Image from "next/image";
 
-export default function HeroCard({ post }) {
+export default function HeroCard() {
   return (
     <section className="flex flex-col md:flex-row gap-4 mt-10 mb-1 mx-auto text-text">
       <div className="flex flex-col gap-4 p-4 md:w-2/5">
-        <h1 className="text-h1 text-display text-center"> {/* TODO, add gradient to span */}
+        <h1 className="text-h1 text-display text-center">
           Vinkit, niksit ja tekniikat <span className="bg-gradient-to-r from-accent-purple to-white bg-clip-text text-transparent">frontend-kehitykseen</span>
         </h1>
         <h3 className="text-white mx-auto text-base text-center xl:text-h4 2xl:text-h5 leading-tight">
           Käsinpoimittuja vinkkejä uusista ja ajankohtaisista aiheista
         </h3>
-        {/* <span className="text-bg rounded-lg font-semibold bg-gradient-to-br from-gradient to-primary group sm:px-5 sm:py-3 px-3 py-2 flex items-center justify-center flex-shrink-0 transition-all duration-200 text-base xl:text-h6 2xl:text-h5">
-          <Link href={`/tip/${post.slug}`} className="text-white">
-            Viimeisin vinkki
-          </Link>
-        </span> */}
       </div>
       <figure className="flex-1">
         <div className="hero-img-container relative hidden xl:block">
--- components/Card/PostCard.js
+++ components/Card/PostCard.js
@@ -28,8 +28,8 @@ export default function PostCard({ post }) {
             <p className="text-light-gray text-base mb-2">{post.description}</p>
           </div>
           <div className="px-6 py-4 flex justify-between items-center">
-            <span className="font-bold text-dark-charcoal bg-accent-purple p-1 rounded-md">
-              Read more
+            <span className="font-bold text-dark-charcoal bg-accent-purple p-2 rounded-md">
+              Lue lisää
             </span>
             <div className="px-3 py-1 rounded-full text-sm">
               <p className="text-gray-500 text-sm">{formattedDate}</p>
